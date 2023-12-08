19b6d818b76cc547343d195ce7ae88e67fcd62fd
miasilvia <135966912+miasilvia@users.noreply.github.com>
2023-12-08 22:16:32 +0700

    router checkout

--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,13 @@ const userRoutes = require('./userroutes')
 const productRoutes = require('./productroutes')
 const cartRoutes = require('./cartroutes')
 const wishlistRoutes = require('./wishlistroutes')
+const checkoutRouts = require('./checkoutRoutes')
 
 router.use('/user', userRoutes )
 router.use('/product', productRoutes )
 router.use('/cart', cartRoutes)
 router.use('/wishlist', wishlistRoutes)
+router.use('/checkout', checkoutRouts)
 
 
 module.exports = router
