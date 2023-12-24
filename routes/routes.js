6de9c3e996e5aac23ce4b3a708fc2e29d3718394
miasilvia <135966912+miasilvia@users.noreply.github.com>
2023-12-24 16:09:04 +0700

    menambah route review

--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,22 @@
-const router    = require('express').Router()
-const userRoutes = require('./userroutes')
-const productRoutes = require('./productroutes')
-const cartRoutes = require('./cartroutes')
-const wishlistRoutes = require('./wishlistroutes')
-const checkoutRouts = require('./checkoutRoutes')
-const paymentRoutes = require('./paymentRoutes')
-const articleRoutes = require('./articleRoutes')
-const adminRoutes = require('./adminRoutes')
+const router = require("express").Router();
+const userRoutes = require("./userroutes");
+const productRoutes = require("./productroutes");
+const cartRoutes = require("./cartroutes");
+const wishlistRoutes = require("./wishlistroutes");
+const checkoutRouts = require("./checkoutRoutes");
+const paymentRoutes = require("./paymentRoutes");
+const articleRoutes = require("./articleRoutes");
+const reviewProduct = require("./reviewProductRoutes");
+const adminRoutes = require("./adminRoutes");
 
-router.use('/user', userRoutes )
-router.use('/product', productRoutes )
-router.use('/cart', cartRoutes)
-router.use('/wishlist', wishlistRoutes)
-router.use('/checkout', checkoutRouts)
-router.use('/payment', paymentRoutes)
-router.use('/article', articleRoutes)
-router.use('/admin', adminRoutes)
+router.use("/user", userRoutes);
+router.use("/product", productRoutes);
+router.use("/cart", cartRoutes);
+router.use("/wishlist", wishlistRoutes);
+router.use("/checkout", checkoutRouts);
+router.use("/payment", paymentRoutes);
+router.use("/article", articleRoutes);
+router.use("/review", reviewProduct);
+router.use("/admin", adminRoutes);
 
-module.exports = router
+module.exports = router;
