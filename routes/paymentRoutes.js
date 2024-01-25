0264c85e1af9e3acb67f402dc5af51d754c3ee74
Nurul Huda Sulton <66205433+nhsltn@users.noreply.github.com>
2024-01-25 23:03:24 +0700

    Delete routes directory

--- routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-global.query = require("../model/query");
-global.response = require("../response/response");
-const payment = require("../controllers/paymentController");
-
-router.post("/process-payment/:id", payment.processPayment);
-router.get("/get/:id", payment.getCheckoutData);
-
-module.exports = router;
--- routes/trackingOrderRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-global.query = require("../model/query");
-global.response = require("../response/response");
-const trackingOrder = require("../controllers/trackingOrderController")
-
-router.post("/update-order-status", trackingOrder.updateOrderStatus);
-
-module.exports = router;
